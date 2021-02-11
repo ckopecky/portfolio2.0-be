4a68009d39f78c14331b5b35ef0755c720821cb2
ckopecky <kopecky12112@gmail.com>
2021-02-11 10:06:49 -0800

    add to cors origin

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose
 	});
 
 const corsOptions = {
-	origin: ["http://localhost:3000", "https://www.christinakopecky.com"]
+	origin: ["http://localhost:3000", "https://www.christinakopecky.com", "https://christina-kopecky.christina.vercel.app", "https://christina-kopecky.christina.vercel.app", "https://christina-kopecky.vercel.app", "http://www.christinakopecky.com", "https://christina-kopecky-q6ztephjk.vercel.app"]
 }
 
 server.use(express.json());
